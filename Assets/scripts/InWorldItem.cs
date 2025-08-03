74d538e095847e4fa3a85dd966f32640168b18a6
stickbrime <jnwwwwwww@outlook.com>
2025-08-03 20:07:12 +0800

    g

--- Assets/scripts/InWorldItem.cs
+++ Assets/scripts/InWorldItem.cs
@@ -6,7 +6,7 @@ public class InWorldItem : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("player"))
+        if (collision.CompareTag("Player"))
         {
             collision.GetComponent<PlayerInventory>().AddItem(item);
             Destroy(gameObject); 
